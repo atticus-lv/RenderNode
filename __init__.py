77a3e88a6e5c4982245caf95fdfa156c3706e9d1
atticus note book <1029910278@qq.com>
2020-11-28 20:52:29 +0800

    improve register

--- __init__.py
+++ __init__.py
@@ -1,57 +1,46 @@
 bl_info = {
-    "name"       : "RenderStack Node",
-    "author"     : "Atticus",
-    "version"    : (0, 3),
-    "blender"    : (2, 90, 0),
-    "location"   : "Node Editor",
+    "name": "RenderStack Node",
+    "author": "Atticus",
+    "version": (0, 4),
+    "blender": (2, 90, 0),
+    "location": "Node Editor",
     "description": "Node based render queue workflow",
     # "doc_url"    : "",
-    "category"   : "Render",
+    "category": "Render",
 }
 
-import os
 import importlib
 import sys
 
-from RenderStackNode.node_tree import RenderStackNode
-from RenderStackNode import nodes
-from RenderStackNode import node_tree
+from .nodes import *
 
 __dict__ = {}
-__dict__["node_tree"] = "RenderStackNode.node_tree"
+__dict__["node_tree"] = f"{__name__}.node_tree"
 
-a = nodes.a
+a = automatic_collection()
+
 for k, v in a.items():
     for module_name in v:
-        __dict__[module_name] = f"RenderStackNode.nodes.{k}.{module_name}"
+        __dict__[module_name] = f"{__name__}.nodes.{k}.{module_name}"
 
-# print(__dict__)
-
-for full_name in __dict__.values():
-    if full_name in sys.modules:
-        importlib.reload(sys.modules[full_name])
+for name in __dict__.values():
+    if name in sys.modules:
+        importlib.reload(sys.modules[name])
     else:
-        globals()[full_name] = importlib.import_module(full_name)
-        setattr(globals()[full_name], 'modules', __dict__)
+        globals()[name] = importlib.import_module(name)
+        setattr(globals()[name], 'modules', __dict__)
 
 
 def register():
     for name in __dict__.values():
         if name in sys.modules and hasattr(sys.modules[name], 'register'):
-            try:
-                sys.modules[name].register()
-                print(f"{name} register !")
-            except Exception as e:
-                print(f"{name} register failed: {e}")
+            sys.modules[name].register()
 
 
 def unregister():
     for name in __dict__.values():
         if name in sys.modules and hasattr(sys.modules[name], 'unregister'):
-            try:
-                sys.modules[name].unregister()
-            except Exception as e:
-                print(f"{name} unregister failed: {e}")
+            sys.modules[name].unregister()
 
 
 if __name__ == '__main__':
