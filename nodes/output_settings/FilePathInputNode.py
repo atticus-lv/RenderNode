bafd0cc00a3dc78ac05b54b68f6d47328644fca8
Atticus_id <1029910278@qq.com>
2021-02-17 16:58:48 +0800

    path expression helper

--- nodes/output_settings/FilePathInputNode.py
+++ nodes/output_settings/FilePathInputNode.py
@@ -25,16 +25,26 @@ class RSNodeFilePathInputNode(RenderStackNode):
     def init(self, context):
         self.outputs.new('RSNodeSocketOutputSettings', "Output Settings")
         self.path_format = get_pref().node_file_path.path_format
-        self.width = 220
+        self.width = 250
 
     def draw_buttons(self, context, layout):
+        # custom path
         layout.prop(self, 'use_blend_file_path')
         if not self.use_blend_file_path:
             row = layout.row(align=1)
             row.prop(self, 'path')
             row.operator('buttons.directory_browse', icon='FILEBROWSER', text='')
-        layout.prop(self, 'path_format', text='')
-
+        # format_name
+        row = layout.row(align=1)
+        row.prop(self, 'path_format', text='')
+        # format_name selector
+        try:
+            if hasattr(bpy.context.space_data, 'edit_tree'):
+                if bpy.context.space_data.edit_tree.nodes.active.name == self.name:
+                    row.menu(menu='RSN_MT_FormatNameMenu', text='', icon='EYEDROPPER')
+        except Exception:
+            pass
+        # viewer node tips
         pref = get_pref()
         if not pref.node_viewer.update_path:
             layout.label(text='Update is disable in viewer node', icon='ERROR')
@@ -47,9 +57,58 @@ class RSNodeFilePathInputNode(RenderStackNode):
         return task_data
 
 
+class RSN_OT_AddFormatName(bpy.types.Operator):
+    bl_idname = "rsn.add_format_name"
+    bl_label = "Add Format Name"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    format_name: StringProperty(default='')
+
+    def execute(self, context):
+        try:
+            node = context.space_data.edit_tree.nodes.active
+            if hasattr(node, 'path_format'):
+                node.path_format += self.format_name
+        except:
+            pass
+
+        return {"FINISHED"}
+
+
+format_names = {
+    'File Name'          : '$blend',
+    'Task Label'         : '$label',
+    'Render Engine'      : '$engine',
+    'Camera Name'        : '$camera',
+    'Resolution: XxY'    : '$res',
+    'Exposure Value'     : '$ev',
+    'View Layer'         : '$vl',
+    'Date: month-day'    : '$data',
+    'Time: minute-second': '$time',
+}
+
+
+class RSN_MT_FormatNameMenu(bpy.types.Menu):
+    bl_label = "FormatName Selector"
+
+    def draw(self, context):
+        layout = self.layout
+
+        for k, v in format_names.items():
+            layout.operator("rsn.add_format_name", text=k).format_name = v
+
+        layout.separator()
+        layout.operator("rsn.add_format_name", text="Creat Folder",
+                        icon="NEWFOLDER").format_name = '/'
+
+
 def register():
     bpy.utils.register_class(RSNodeFilePathInputNode)
+    bpy.utils.register_class(RSN_OT_AddFormatName)
+    bpy.utils.register_class(RSN_MT_FormatNameMenu)
 
 
 def unregister():
     bpy.utils.unregister_class(RSNodeFilePathInputNode)
+    bpy.utils.unregister_class(RSN_OT_AddFormatName)
+    bpy.utils.unregister_class(RSN_MT_FormatNameMenu)
