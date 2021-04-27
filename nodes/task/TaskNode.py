1be0934ba4f2b3781584c32b7f4a4160bc320e1e
Atticus_id <1029910278@qq.com>
2021-04-27 11:53:46 +0800

    remove old code

--- nodes/task/TaskNode.py
+++ nodes/task/TaskNode.py
@@ -25,7 +25,7 @@ class RSNodeTaskNode(RenderStackNode):
         self.outputs.new('RSNodeSocketRenderList', "Task")
         self.label = self.name
 
-        bpy.ops.rsn.edit_var_collect(action="ADD", task_node_name=self.name)
+        # bpy.ops.rsn.edit_var_collect(action="ADD", task_node_name=self.name)
 
     def draw_buttons(self, context, layout):
         layout.use_property_split = 1
