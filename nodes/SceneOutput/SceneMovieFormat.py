de5f54623087464e6259e7996c3f587d38d4e7cc
atticus-lv <64118610+atticus-lv@users.noreply.github.com>
2021-09-07 22:58:50 +0800

    add fps to movie format node

--- nodes/SceneOutput/SceneMovieFormat.py
+++ nodes/SceneOutput/SceneMovieFormat.py
@@ -22,7 +22,20 @@ def enum_ffmpeg_preset():
     return enums
 
 
+def enum_frame_rate_preset():
+    ffmpeg_preset_dir = os.path.join(get_preset_folder(), 'framerate')
+    enums = [(file_name, file_name[0:-3], '') for file_name in os.listdir(ffmpeg_preset_dir)]
+    return enums
+
+
 def update_node(self, context):
+    if self.use_frame_rate_preset:
+        self.remove_input('fps')
+        self.remove_input('frame_base')
+    else:
+        self.create_input('RenderNodeSocketInt', 'fps', 'FPS', default_value=24)
+        self.create_input('RenderNodeSocketFloat', 'frame_base', 'Base', default_value=1.00)
+
     self.execute_tree()
 
 
@@ -47,8 +60,8 @@ class RenderNodeSceneMovieFormat(RenderNodeBase):
                          default=90, min=0, max=100,
                          update=update_node)
 
-    use_ffmpeg_preset: BoolProperty(name='Use Preset', default=False)
-    ffmpeg_preset: EnumProperty(name='Preset', items=[
+    use_ffmpeg_preset: BoolProperty(name='Use FFMPEG Preset', default=False)
+    ffmpeg_preset: EnumProperty(name='FFMPEG Preset', items=[
         ('DVD_(note_colon__this_changes_render_resolution).py', 'DVD_(note_colon__this_changes_render_resolution)', ''),
         ('H264_in_Matroska.py', 'H264_in_Matroska', ''),
         ('H264_in_Matroska_for_scrubbing.py', 'H264_in_Matroska_for_scrubbing', ''),
@@ -57,8 +70,21 @@ class RenderNodeSceneMovieFormat(RenderNodeBase):
                                 default='H264_in_MP4.py',
                                 update=update_node)
 
+    use_frame_rate_preset: BoolProperty(name='Use Frame Preset', default=True, update=update_node)
+    frame_rate_preset: EnumProperty(name='FFMPEG Preset', items=[
+        ('23.98.py', '23.98', ''),
+        ('24.py', '24', ''),
+        ('29.97.py', '29.97', ''),
+        ('30.py', '30', ''),
+        ('50.py', '50', ''),
+        ('59.94.py', '59.94', ''),
+        ('60.py', '60', ''),
+        ('120.py', '120', ''),
+        ('240.py', '240', ''), ],
+                                    default='24.py', update=update_node)
+
     def init(self, context):
-        self.create_output('RSNodeSocketTaskSettings','Settings','Settings')
+        self.create_output('RSNodeSocketTaskSettings', 'Settings', 'Settings')
         self.width = 200
 
     def draw_buttons(self, context, layout):
@@ -75,18 +101,28 @@ class RenderNodeSceneMovieFormat(RenderNodeBase):
             col.prop(self, 'use_ffmpeg_preset')
             if self.use_ffmpeg_preset: col.prop(self, 'ffmpeg_preset')
 
-    def process(self,context,id,path):
-        attr_list = ['file_format',
-                     'quality', ]
+        col.prop(self, 'use_frame_rate_preset')
+        if self.use_frame_rate_preset: col.prop(self, 'frame_rate_preset')
+
+    def process(self, context, id, path):
+        attr_list = ['file_format','quality', ]
+
         for attr in attr_list:
             try:
                 self.compare(bpy.context.scene.render.image_settings, attr, getattr(self, attr))
             except Exception as e:
                 print(e)
 
-        if self.file_format == 'FFMPEG' and self.use_ffmpeg_preset:
-            ffmpeg_preset_dir = os.path.join(get_preset_folder(), 'ffmpeg')
-            bpy.ops.script.python_file_run(filepath=os.path.join(ffmpeg_preset_dir, self.ffmpeg_preset))
+        if self.file_format == 'FFMPEG':
+            if self.use_ffmpeg_preset:
+                ffmpeg_preset_dir = os.path.join(get_preset_folder(), 'ffmpeg')
+                bpy.ops.script.python_file_run(filepath=os.path.join(ffmpeg_preset_dir, self.ffmpeg_preset))
+            if self.use_frame_rate_preset:
+                frame_rate_preset_dir = os.path.join(get_preset_folder(), 'framerate')
+                bpy.ops.script.python_file_run(filepath=os.path.join(frame_rate_preset_dir, self.frame_rate_preset))
+            else:
+                context.scene.render.fps = self.inputs['fps'].get_value()
+                context.scene.render.frame_base = self.inputs['frame_base'].get_value()
 
 
 def register():
