dfb8e434595c90484366f389e1da1bab9e17b8e9
atticus-lv <1029910278@qq.com>
2021-12-29 13:01:57 +0800

    add set all collection visibility socket

--- nodes/Collection/SetCollectionVisibility.py
+++ nodes/Collection/SetCollectionVisibility.py
@@ -9,6 +9,8 @@ class RenderNodeSetCollectionVisibility(RenderNodeBase):
 
     def init(self, context):
         self.create_input('RenderNodeSocketTask', 'task', 'Task')
+        self.create_input('RenderNodeSocketBool', 'set_all', 'Set All Collections', default_value=False)
+
         self.create_input('RenderNodeSocketCollection', 'collection', '', show_text=False)
         self.create_input('RenderNodeSocketBool', 'hide_viewport', 'Show In Viewports',
                           default_value=True)  # invert bool
@@ -18,13 +20,25 @@ class RenderNodeSetCollectionVisibility(RenderNodeBase):
 
     def process(self, context, id, path):
         if not self.process_task(): return
-        coll = self.inputs['collection'].get_value()
+        set_all = self.inputs.get('set_all')
         hide_viewport = self.inputs['hide_viewport'].get_value()
         hide_render = self.inputs['hide_render'].get_value()
-        if hide_viewport is None or hide_render is None or coll is None: return
-        # invert
-        coll.hide_viewport = not hide_viewport
-        coll.hide_render = not hide_render
+
+        coll = self.inputs['collection'].get_value()
+        if hide_viewport is None or hide_render is None or (coll is None and not set_all): return
+
+        def set_coll(coll, hide_viewport, hide_render):
+            # invert
+            coll.hide_viewport = not hide_viewport
+            coll.hide_render = not hide_render
+
+        if set_all and set_all.get_value() is True:
+            self.inputs['collection'].hide = True
+            for coll in bpy.context.scene.collection.children:
+                set_coll(coll, hide_viewport, hide_render)
+        else:
+            self.inputs['collection'].hide = False
+            set_coll(coll, hide_viewport, hide_render)
 
 
 def register():
